f6c56fdecec8829b0cd7de6a13322d657848f7e8
Henrique Simões <henrique_barros_simoes@hotmail.com>
2024-10-07 15:07:45 -0300

    VARIANT_USART

--- Inc/config.h
+++ Inc/config.h
@@ -10,7 +10,6 @@
 // Ubuntu: define the desired build variant here if you want to use make in console
 // or use VARIANT environment variable for example like "make -e VARIANT=VARIANT_NUNCHUK". Select only one at a time.
 
-#define VARIANT_USART
 
 #if !defined(PLATFORMIO)
   //#define VARIANT_ADC         // Variant for control via ADC input
